9943d8734e7584d11d9810a069c0766bfeac0ed0
Gavin Sadler <35090401+GavinSadler@users.noreply.github.com>
2026-07-03 15:15:38 -0400

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+
+    int dfs(int i, bool inSubarray, vector<int>& nums) {
+        int n = nums.size();
+
+        // Last item in the array
+        if (i == n - 1) {
+            if (inSubarray)
+                return max(0, nums[i]);
+            else
+                return nums[i];
+        }
+
+        if (inSubarray)
+            return max(
+                0, 
+                nums[i] + dfs(i + 1, true, nums)
+            );
+
+        return max(
+            dfs(i + 1, false, nums),
+            nums[i] + dfs(i + 1, true, nums)
+        );
+    }
+
+public:
+    int maxSubArray(vector<int>& nums) {
+        return dfs(0, false, nums);
+    }
+};
