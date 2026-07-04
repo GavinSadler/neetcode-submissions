0abdf79a6917a601f196e3bc6f6429b159a59414
Gavin Sadler <35090401+GavinSadler@users.noreply.github.com>
2026-07-04 14:11:54 -0400

    Add: maximum-subarray - submission-6

---
+++ Data Structures & Algorithms/maximum-subarray/submission-6.cpp
@@ -0,0 +1,64 @@
+class Solution {
+    int rs(int i, bool inSubarray, vector<int>& nums) {
+        int n = nums.size();
+        
+        if (i == n - 1) {
+            if (!inSubarray) {
+                return nums[i];
+            } else {
+                return max(nums[i], 0);
+            }
+        }
+
+        if (!inSubarray){
+            return max(
+                rs(i + 1, false, nums),
+                nums[i] + rs(i + 1, true, nums)
+            );
+        }
+
+        return max(
+            nums[i] + rs(i + 1, true, nums),
+            0
+        );
+    }
+
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        // vector<vector<int>> dp(2, vector(nums.size(), 0));
+
+        // dp[0][n - 1] = nums[n - 1];
+        // dp[1][n - 1] = max(0, nums[n - 1]);
+
+        int top    = nums[n - 1];
+        int bottom = max(0, nums[n - 1]);
+
+        for(int i = n - 2; i >= 0; i--) {
+            // dp[0][i] = max(
+            //     dp[0][i + 1],
+            //     nums[i] + dp[1][i + 1]
+            // );
+
+            top = max(
+                top,
+                nums[i] + bottom
+            );
+
+            // dp[1][i] = max(
+            //     nums[i] + dp[1][i + 1],
+            //     0
+            // );
+
+            bottom = max(
+                nums[i] + bottom,
+                0
+            );
+        }
+
+
+        return top;
+    }
+};
